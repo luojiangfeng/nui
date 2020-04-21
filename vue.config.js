3224dc0493c14ca0319e88d70df64a1015e413e8
“luojiangfeng” <416829980@qq.com>
2020-04-21 14:29:07 +0800

    webpack

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 const port = 8090
 
 module.exports = {
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     const oneOfsMap = config.module.rule("scss").oneOfs.store
-    oneOfsMap.forEach(item => {
+    oneOfsMap.forEach((item) => {
       item
         .use("sass-resources-loader")
         .loader("sass-resources-loader")
@@ -15,8 +15,8 @@ module.exports = {
           resources: [
             "./src/assets/styles/_var.scss",
             "./src/assets/styles/_mixin.scss",
-            "./src/assets/styles/_animation.scss"
-          ]
+            "./src/assets/styles/_animation.scss",
+          ],
         })
         .end()
     })
@@ -29,20 +29,20 @@ module.exports = {
   // publicPath: '/',//部署应用包时的基本 URL
   // productionSourceMap: isProduction ? false : true, // 解决vue项目打包后浏览器F12查看到项目源代码false不能看到
   // productionSourceMap: true, // 测试调试打断点
-  lintOnSave: false // 去掉eslint校验
-  // devServer: {
-  //   port: port, // 设置端口号
-  //   open: true, // 启动项目自动打开浏览器
-  //   proxy: {
-  //     // 解决跨域问题
-  //     "/api": {
-  //       target: `http://localhost:${port}/`, // 本地json数据
-  //       changeOrigin: true,
-  //       ws: false,
-  //       pathRewrite: {
-  //         "/api": "/mock" // 本地
-  //       }
-  //     }
-  //   }
-  // }
+  lintOnSave: false, // 去掉eslint校验
+  devServer: {
+    port: port, // 设置端口号
+    open: true, // 启动项目自动打开浏览器
+    // proxy: {
+    //   // 解决跨域问题
+    //   "/api": {
+    //     target: `http://localhost:${port}/`, // 本地json数据
+    //     changeOrigin: true,
+    //     ws: false,
+    //     pathRewrite: {
+    //       "/api": "/mock" // 本地
+    //     }
+    //   }
+    // }
+  },
 }
